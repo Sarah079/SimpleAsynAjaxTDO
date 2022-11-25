6b7d2dec3e15cc6e1e8f5a0493b1207cc7e5d5f7
Sarah079 <u19032618@tuks.co.za>
2022-11-25 21:27:19 +0200

    Add project files.

---
+++ SimpleAsynAjax/Controllers/HomeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using SimpleAsynAjax.Models;
+using Newtonsoft.Json;
+using System.Net;
+using System.Data;
+using System.Threading;
+using System.Web.Services.Description;
+
+
+namespace SimpleAsynAjax.Controllers
+    {
+
+    public class HomeController : Controller
+        {
+
+        public ExampleDatabaseEntities db = new ExampleDatabaseEntities();
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ExampleTable tbl)
+        {
+            var message = "";
+            try
+            {
+                db.ExampleTables.Add(tbl); db.SaveChanges();
+                message = "Success";
+            }
+            catch (Exception msg)
+            {
+                message = msg.Message;
+            }
+            return Json(new { Message = message, JsonRequestBehavior.AllowGet });
+        }
+
+        public JsonResult GetExampleTable(ExampleTable obj)
+        {
+            List<ExampleTable> tables = new List<ExampleTable>(); 
+            tables = db.ExampleTables.ToList();
+            return Json(tables, JsonRequestBehavior.AllowGet);
+        }
+    }
+   
+}
+
+
